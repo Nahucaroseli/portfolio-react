5803475b7642e4f7619946b587bb621d7855fd99
Nahuel Caroseli <nahucaroseli2100@gmail.com>
2025-10-15 17:23:47 -0300

    modificado algunos estilos y experiencia

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -7,24 +7,6 @@ function Projects() {
 
       <div className="w-full flex flex-col items-center pt-1">
         <div className="flex flex-col md:pl-3 gap-8 pt-4 w-full items-center justify-center">
-          <div className="flex flex-col w-full max-w-[70rem] h-[500px] md:h-[600px] shadow-lg rounded-lg justify-between" id="project_1">
-            <div className="p-4 text-white flex flex-col">
-            <h1 className="text-white text-3xl md:text-6xl  font-bold">COMPRA FACIL</h1>
-              <h2 className="text-lg mt-10 font-bold">React.js</h2>
-              <h2 className="text-lg font-bold">TailwindCSS</h2>
-              <h2 className="text-lg font-bold">MongoDB</h2>
-              <h2 className="text-lg font-bold">React Hot Toast</h2>
-              <h2 className="text-lg font-bold">Node.js</h2>
-              <h2 className="text-lg font-bold">Express.js</h2>
-              <h2 className="text-lg font-bold">Mongoose</h2>
-              <h2 className="text-lg font-bold">JWT</h2>
-            </div>
-            <div className='p-4 flex flex-row text-white gap-x-5'>
-                <a href="https://github.com/Nahucaroseli/e-commerce-react" target='_blank' className='md:text-xl font-bold underline'>FRONTEND CODE</a>
-                <a href="https://github.com/Nahucaroseli/e-commerce-backend" target='_blank' className='md:text-xl font-bold underline'>BACKEND CODE</a>
-                <a href="https://compra-facil.netlify.app/" target='_blank' className='md:text-xl font-bold underline'>WEBSITE</a>
-            </div>
-          </div>
           <div className="flex flex-col w-full max-w-[70rem] h-[500px] md:h-[600px] shadow-lg rounded-lg justify-between" id="project_3">
 
             <div className="p-4 text-white flex flex-col">
@@ -49,6 +31,10 @@ function Projects() {
               <h2 className="text-lg font-bold">Node.js</h2>
               <h2 className="text-lg font-bold">Express.js</h2>
               <h2 className="text-lg font-bold">Brevo API</h2>
+              <h2 className="text-lg font-bold">Vimeo API</h2>
+              <h2 className="text-lg font-bold">Mercado Pago Implementation</h2>
+              <h2 className="text-lg font-bold">Lucide</h2>
+              <h2 className="text-lg font-bold">Lenis</h2>
             </div>
             <div className='p-4 flex flex-row text-white gap-x-5'>
                 <a href="https://nicotrainer.pages.dev/" target='_blank' className='md:text-xl font-bold underline'>WEBSITE</a>
@@ -57,19 +43,21 @@ function Projects() {
           <div className="flex flex-col w-full max-w-[70rem] h-[500px] md:h-[600px] shadow-lg rounded-lg justify-between" id="project_5">
 
             <div className="p-4 text-white flex flex-col">
-            <h1 className="text-white text-3xl md:text-6xl  font-bold">PORTAL EMPLEO LOBERIA</h1>
-              <h2 className="text-lg mt-10 font-bold">Angular</h2>
-              <h2 className="text-lg font-bold">Material</h2>
-              <h2 className="text-lg font-bold">Node.js</h2>
-              <h2 className="text-lg font-bold">Express.js</h2>
-              <h2 className="text-lg font-bold">Supabase</h2>
-              <h2 className="text-lg font-bold">JWT</h2>
-              <h2 className="text-lg font-bold">PostgreSQL</h2>
-              <h2 className="text-lg font-bold">PDFKit</h2>
-              <h2 className="text-lg font-bold">BCrypt</h2>
-              <h2 className="text-lg font-bold">Joi</h2>
-              <h2 className="text-lg font-bold">Multer</h2>
-              <h2 className="text-lg font-bold">Nodemailer</h2>
+              <h1 className="text-white text-3xl md:text-6xl  font-bold">PORTAL EMPLEO LOBERIA</h1>
+              <div className='flex flex-col h-[21rem] md:h-full -mt-5'>
+                <h2 className="text-lg mt-10 font-bold">Angular</h2>
+                <h2 className="text-lg font-bold">Material</h2>
+                <h2 className="text-lg font-bold">Node.js</h2>
+                <h2 className="text-lg font-bold">Express.js</h2>
+                <h2 className="text-lg font-bold">Supabase</h2>
+                <h2 className="text-lg font-bold">JWT</h2>
+                <h2 className="text-lg font-bold">PostgreSQL</h2>
+                <h2 className="text-lg font-bold">PDFKit</h2>
+                <h2 className="text-lg font-bold">BCrypt</h2>
+                <h2 className="text-lg font-bold">Joi</h2>
+                <h2 className="text-lg font-bold">Multer</h2>
+                <h2 className="text-lg font-bold">Nodemailer</h2>
+              </div>
             </div>
             <div className='p-4 flex flex-row text-white gap-x-5'>
                 <a href="https://empleo.loberia.gob.ar" target='_blank' className='md:text-xl font-bold underline'>WEBSITE</a>
